342d7d3de02ee42978ffb674e6adf0517866e877
Michael Harris <xero5k@yahoo.com>
2021-07-10 08:27:22 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const check2 = document.body.querySelector('main');
+check2.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.innerHTML = "YOUR-NAME is the champion";
+newHeader.id = "victory";
